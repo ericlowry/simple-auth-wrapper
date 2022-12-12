2d253a520bd1002c46ebcf4c05e336cd6f4a0e74
Eric Lowry <eric@myrealms.org>
2022-12-12 15:26:26 -0700

    fetchOptions cleanup

--- client/src/state/useAuth.js
+++ client/src/state/useAuth.js
@@ -3,34 +3,12 @@
 //
 import { createContext, useContext, useEffect, useState } from 'react';
 import handleFetchResponse from '../lib/handleFetchResponse';
+import fetchOptions from '../lib/fetchOptions';
 
 const Ctx = createContext();
 
 // on page load, ensure that we don't have a token
 sessionStorage.setItem('accessToken', '');
-
-function fetchOptions(opts = {}) {
-  const { method = 'GET', body, accessToken } = opts;
-  const options = {
-    method,
-    headers: {
-      'Accept': 'application/json',
-    },
-    mode: 'cors',
-    credentials: 'include',
-  };
-
-  if (accessToken) {
-    options.headers['Authorization'] = `Bearer ${accessToken}`;
-  }
-
-  if (body) {
-    options.headers['Content-Type'] = 'application/json';
-    options.body = JSON.stringify(body);
-  }
-
-  return options;
-}
 
 //
 // Provider
